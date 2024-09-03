92907dd268cffd90fe03937f6330bb09b48d9032
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-03 15:26:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+function App() {
+  return (
+    <div>
+      <h1>Gerenciador de Tarefas 📝</h1>
+      <p>
+        Bem-vindo ao seu gerenciador de tarefas. Vamos começ ar a organizar seu
+        dia!
+      </p>
+    </div>
+  )
+}
+export default App
